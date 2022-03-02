d224af073d9b8f4ca4ef7ec4cd2b7e1f84ebcfb3
Dave Keck <dave@heytoaster.com>
2022-03-02 04:01:18 -0800

    Cleanup

--- main.mm
+++ main.mm
@@ -26,34 +26,4 @@ int main(int argc, const char* argv[]) {
         }
     }
     return 0;
-    
-    
-    
-    
-//    bool bColors = b_Colors;
-//    Soft_Label_Key_Set* S = 0;
-//
-//    int ts = titlesize();
-//    if (ts>0) NCursesWindow::ripoffline(ts,rinit);
-//    Soft_Label_Key_Set::Label_Layout fmt = useSLKs();
-//    if (fmt!=Soft_Label_Key_Set::None) {
-//        S = new Soft_Label_Key_Set(fmt);
-//        assert(S != 0);
-//        init_labels(*S);
-//    }
-//
-//    Root_Window = new NCursesWindow(::stdscr);
-//    init(bColors);
-//
-//    if (ts>0) title();
-//    
-//    if (fmt!=Soft_Label_Key_Set::None) {
-//        push(*S);
-//    }
-//    
-//    return run();
-    
-    
-    
-    return 0;
 }
