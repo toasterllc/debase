232e8c9d822dd0d192a2422bbac3d5aed2c3eb95
Dave Keck <dave@heytoaster.com>
2022-03-02 04:00:31 -0800

    Progress

--- main.mm
+++ main.mm
@@ -1,13 +1,30 @@
 #include <iostream>
-#include "curses.h"
-//#include "ncurses/c++/cursesapp.h"
-#include "ncurses/c++/cursslk.h"
-#include "ncurses/c++/cursesm.h"
-#include "ncurses/c++/cursesf.h"
+#include <unistd.h>
+#include "ncurses/c++/cursesp.h"
 
 int main(int argc, const char* argv[]) {
-    NCursesWindow win(::stdscr);
+    // There doesn't seem to be a better way to do this
+    setenv("TERM", "xterm-1003", true);
     ::endwin();
+    
+    NCursesWindow win(::stdscr);
+    NCursesPanel panel(5, 10);
+    panel.printw(1, 1, "hello");
+    panel.box();
+    
+    mousemask(ALL_MOUSE_EVENTS | REPORT_MOUSE_POSITION, NULL);
+    mouseinterval(0);
+    
+    for (;;) {
+        NCursesPanel::redraw();
+        int key = win.getch();
+        if (key == KEY_MOUSE) {
+            MEVENT mouseEvent = {};
+            int ir = getmouse(&mouseEvent);
+            if (ir != OK) continue;
+            panel.mvwin(mouseEvent.y, mouseEvent.x);
+        }
+    }
     return 0;
     
     
