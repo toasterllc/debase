5ed211d4cf188aeefb76f810b8cdaa0f03ff1733
Dave <dave@heytoaster.com>
2022-03-31 15:51:59 -0700

    Reverse completed todo list so we don't have to scroll so much

--- src/main.cpp
+++ src/main.cpp
@@ -1400,8 +1400,6 @@ static void _PrintUsage() {
 }
 
 int main(int argc, const char* argv[]) {
-    #warning TODO: TextField: make text truncation work, including arrow keys to change left position
-    
     #warning TODO: TextField: filter printable characters better
     
     #warning TODO: TextField: don't lose content when resizing window
@@ -1431,183 +1429,184 @@ int main(int argc, const char* argv[]) {
     #warning TODO: figure out why moving/copying commits is slow sometimes
     
 //  DONE:
-//    #warning TODO: when copying commmits, don't hide the source commits
-//    
-//    #warning TODO: allow escape key to abort a drag
-//    
-//    #warning TODO: we need to unique-ify the supplied revs, since we assume that each column has a unique rev
-//    #warning       to do so, we'll need to implement operator< on Rev so we can put them in a set
-//    
-//    #warning TODO: show similar commits to the selected commit using a lighter color
-//    
-//    #warning TODO: don't allow remote branches to be modified (eg 'origin/master')
-//    
-//    #warning TODO: create concept of revs being mutable. if they're not mutable, don't allow moves from them (only copies), moves to them, deletes, etc
-//    
-//    #warning TODO: draw "Move/Copy" text immediately above the dragged commits, instead of at the insertion point
-//
-//    #warning TODO: implement CombineCommits
-//
-//    #warning TODO: add affordance to cancel current drag by moving mouse to edge of terminal
-//
-//    #warning TODO: improve panel dragging along edges. currently the shadow panels get a little messed up
-//
-//    #warning TODO: fix commit message rendering when there are newlines
-//    
-//    #warning TODO: configure tty before/after calling out to a new editor
-//    
-//    #warning TODO: allow editing author/date of commit
-//
-//    #warning TODO: implement EditCommit
-//
-//    #warning TODO: special-case opening `mate` when editing commit, to not call CursesDeinit/CursesInit
-//
-//    #warning TODO: show indication in the UI that a column is immutable
-//
-//    #warning TODO: implement key combos for combine/edit
-//
-//    #warning TODO: implement double-click to edit
-//
-//    #warning TODO: double-click broken: click commit, wait, then double-click
-//
-//    #warning TODO: implement error messages
-//
-//    #warning TODO: figure out whether/where/when to call git_libgit2_shutdown()
-//
-//    #warning TODO: fix: colors aren't restored when exiting
-//
-//    #warning TODO: if can_change_color() returns false, use default color palette (COLOR_RED, etc)
-//
-//    #warning TODO: create special color palette for apple terminal
-//    
-//    #warning TODO: handle rewriting tags
-//
-//    #warning TODO: don't allow double-click/return key on commits in read-only columns
-//
-//    #warning TODO: re-evaluate size of drag-cancel affordance since it's not as reliable in iTerm
-//
-//    #warning TODO: handle window resizing
-//
-//    #warning TODO: show some kind of indication that a commit is a merge commit
-//
-//    #warning TODO: properly handle moving/copying merge commits
-//
-//    #warning TODO: make sure moving/deleting commits near the root commit still works
-//    
-//    #warning TODO: always show the same contextual menu, but gray-out the disabled options
-//
-//    #warning TODO: don't allow combine when a merge commit is selected
-//
-//    #warning TODO: set_escdelay: not sure if we're going to encounter issues?
-//
-//    #warning TODO: handle merge conflicts
-//
-//    #warning TODO: bring back _CommitTime so we don't need to worry about the 'sign' field of git_time
-//    
-//    #warning TODO: make sure we can round-trip with the same date/time. especially test commits with negative UTC offsets!
-//
-//    #warning TODO: no-op guarantee:
-//    #warning TODO:   same commit message -> no repo changes
-//    #warning TODO:   dragging commits to their current location -> no repo changes
-//
-//    #warning TODO: improve error messages: merge conflicts, deleting last branch commit
-//
-//    #warning TODO: when supplying refs on the command line in the form ref^ or ref~N, can we use a ref-backed rev (instead of using a commit-backed rev), and just offset the RevColumn, so that the rev is mutable?
-//
-//    #warning TODO: improve error messages when we can't lookup supplied refs
-//
-//    #warning TODO: fix wrong column being selected after moving commit in master^
-//
-//    #warning TODO: make sure debase still works when running on a detached HEAD
-//    
-//    #warning TODO: make "(HEAD)" suffix persist across branch modifications
-//
-//    #warning TODO: undo: fix UndoHistory deserialization
-//    
-//    #warning TODO: when deserializing, if a ref doesn't exist, don't throw an exception, just prune the entry
-//
-//    #warning TODO: support undo/redo
-//
-//    #warning TODO: fix: copying a commit from a column shouldn't affect column's undo state (but it does)
-//    
-//    #warning TODO: fix: (1) copy a commit to col A, (2) swap elements 1 & 2 of col A. note how the copied commit doesn't get selected when performing undo/redo
-//    
-//    #warning TODO: undo: remember selection as a part of the undo state
-//
-//    #warning TODO: implement _ConfigDir() for real
-//
-//    #warning TODO: perf: if we aren't modifying the current checked-out branch, don't detach head
-//
-//    #warning TODO: refuse to run if there are uncomitted changes and we're detaching head
-//
-//    #warning TODO: show detailed error message if we fail to restore head due to conflicts
-//
-//    #warning TODO: don't lose the UndoHistory when: debase is run and a ref points to a different commit than we last observed
-//    #warning TODO: don't lose the UndoHistory when: a ref no longer exists that we previously observed
-//    
-//    #warning TODO: write a version number at the root of the StateDir, and check it somewhere
-//
-//    #warning TODO: use advisory locking in State class to prevent multiple entities from modifying state on disk simultaneously. RepoState class will need to use this facility when writing its state. Whenever we acquire the lock, we should read the version, because the version may have changed since the last read! if so -> bail!
-//
-//    #warning TODO: test a commit stored in the undo history not existing
-//
-//    #warning TODO: nevermind: implement log of events, so that if something goes wrong, we can manually get back
-//    
-//    #warning TODO: backup all supplied revs before doing anything
-//
-//    #warning TODO: turn snapshots into merely keeping track of what commit was the HEAD, not the full undo history.
-//    #warning TODO: when restoring a snapshot, it should just restore the HEAD of the snapshot, and push an undo operation (as any other operation) that the user can undo/redo as normal
-//    #warning TODO: further, snapshots should be created only at startup ("Session Start"), and on exit, the snapshot to only be enqueued if it differs from the current HEAD of the repo
-//
-//    #warning TODO: when creating a new snapshot, if an equivalent one already exists, remove the old one.
-//
-//    #warning TODO: get command-. working while context menu/snapshot menu is open
-//
-//    #warning TODO: fix: crash when deleting prefs dir while debase is running
-//
-//    #warning TODO: show "Session Start" snapshot
-//
-//    #warning TODO: fully flesh out when to create new snapshots
-//
-//    #warning TODO: nevermind: only show the current-session marker on the "Session Start" element if there's no other snapshot that has it
-//
-//    #warning TODO: improve appearance of active snapshot marker in snapshot menu
-//
-//    #warning TODO: nevermind: figure out how to hide a snapshot if it's the same as the "Session Start" snapshot
-//
-//    #warning TODO: keep the oldest snapshots, instead of the newest
-//
-//    #warning TODO: make sure works with submodules
-//
-//    #warning TODO: nevermind: don't show the first snapshot if it's the same as the "Session Start" snapshot
-//
-//    #warning TODO: fix: handle case when snapshot menu won't fit entirely on screen
-//    
-//    #warning TODO: rename all hittest functions -> updateMouse
-//
-//    #warning TODO: nevermind: show warning on startup: Take care when rewriting history. As with any software, debase may be bugs. As a safety precaution, debase will automatically backup all branches before modifying them, as <BranchName>-DebaseBackup
-//
-//    #warning TODO: fix: snapshots line wrap is 1-character off
-//
-//    #warning TODO: move branch name to top
-//
-//    #warning TODO: when switching snapshots, clear the selection
-//
-//    #warning TODO: improve error when working directory isn't a git repo
-//
-//    #warning TODO: support light mode
-//
-//    #warning TODO: make dark-mode error color more reddish
-//
-//    #warning TODO: handle not being able to materialize commits via Convert()
-//
-//    #warning TODO: fix: if the mouse is moving upon exit, we get mouse characters printed to the terminal
-//
-//    #warning TODO: add help flag
+//    #warning TODO: TextField: make text truncation work, including arrow keys to change left position//
+//    #warning TODO: TextField: make enter key work
 //
 //    #warning TODO: TextField: make tabbing between fields work
 //
-//    #warning TODO: TextField: make enter key work
+//    #warning TODO: add help flag
+//
+//    #warning TODO: fix: if the mouse is moving upon exit, we get mouse characters printed to the terminal
+//
+//    #warning TODO: handle not being able to materialize commits via Convert()
+//
+//    #warning TODO: make dark-mode error color more reddish
+//
+//    #warning TODO: support light mode
+//
+//    #warning TODO: improve error when working directory isn't a git repo
+//
+//    #warning TODO: when switching snapshots, clear the selection
+//
+//    #warning TODO: move branch name to top
+//
+//    #warning TODO: fix: snapshots line wrap is 1-character off
+//
+//    #warning TODO: nevermind: show warning on startup: Take care when rewriting history. As with any software, debase may be bugs. As a safety precaution, debase will automatically backup all branches before modifying them, as <BranchName>-DebaseBackup
+//
+//    #warning TODO: rename all hittest functions -> updateMouse
+//    
+//    #warning TODO: fix: handle case when snapshot menu won't fit entirely on screen
+//
+//    #warning TODO: nevermind: don't show the first snapshot if it's the same as the "Session Start" snapshot
+//
+//    #warning TODO: make sure works with submodules
+//
+//    #warning TODO: keep the oldest snapshots, instead of the newest
+//
+//    #warning TODO: nevermind: figure out how to hide a snapshot if it's the same as the "Session Start" snapshot
+//
+//    #warning TODO: improve appearance of active snapshot marker in snapshot menu
+//
+//    #warning TODO: nevermind: only show the current-session marker on the "Session Start" element if there's no other snapshot that has it
+//
+//    #warning TODO: fully flesh out when to create new snapshots
+//
+//    #warning TODO: show "Session Start" snapshot
+//
+//    #warning TODO: fix: crash when deleting prefs dir while debase is running
+//
+//    #warning TODO: get command-. working while context menu/snapshot menu is open
+//
+//    #warning TODO: when creating a new snapshot, if an equivalent one already exists, remove the old one.
+//
+//    #warning TODO: further, snapshots should be created only at startup ("Session Start"), and on exit, the snapshot to only be enqueued if it differs from the current HEAD of the repo
+//    #warning TODO: when restoring a snapshot, it should just restore the HEAD of the snapshot, and push an undo operation (as any other operation) that the user can undo/redo as normal
+//    #warning TODO: turn snapshots into merely keeping track of what commit was the HEAD, not the full undo history.
+//
+//    #warning TODO: backup all supplied revs before doing anything
+//    
+//    #warning TODO: nevermind: implement log of events, so that if something goes wrong, we can manually get back
+//
+//    #warning TODO: test a commit stored in the undo history not existing
+//
+//    #warning TODO: use advisory locking in State class to prevent multiple entities from modifying state on disk simultaneously. RepoState class will need to use this facility when writing its state. Whenever we acquire the lock, we should read the version, because the version may have changed since the last read! if so -> bail!
+//
+//    #warning TODO: write a version number at the root of the StateDir, and check it somewhere
+//    
+//    #warning TODO: don't lose the UndoHistory when: a ref no longer exists that we previously observed
+//    #warning TODO: don't lose the UndoHistory when: debase is run and a ref points to a different commit than we last observed
+//
+//    #warning TODO: show detailed error message if we fail to restore head due to conflicts
+//
+//    #warning TODO: refuse to run if there are uncomitted changes and we're detaching head
+//
+//    #warning TODO: perf: if we aren't modifying the current checked-out branch, don't detach head
+//
+//    #warning TODO: implement _ConfigDir() for real
+//
+//    #warning TODO: undo: remember selection as a part of the undo state
+//    
+//    #warning TODO: fix: (1) copy a commit to col A, (2) swap elements 1 & 2 of col A. note how the copied commit doesn't get selected when performing undo/redo
+//    
+//    #warning TODO: fix: copying a commit from a column shouldn't affect column's undo state (but it does)
+//
+//    #warning TODO: support undo/redo
+//
+//    #warning TODO: when deserializing, if a ref doesn't exist, don't throw an exception, just prune the entry
+//    
+//    #warning TODO: undo: fix UndoHistory deserialization
+//
+//    #warning TODO: make "(HEAD)" suffix persist across branch modifications
+//    
+//    #warning TODO: make sure debase still works when running on a detached HEAD
+//
+//    #warning TODO: fix wrong column being selected after moving commit in master^
+//
+//    #warning TODO: improve error messages when we can't lookup supplied refs
+//
+//    #warning TODO: when supplying refs on the command line in the form ref^ or ref~N, can we use a ref-backed rev (instead of using a commit-backed rev), and just offset the RevColumn, so that the rev is mutable?
+//
+//    #warning TODO: improve error messages: merge conflicts, deleting last branch commit
+//
+//    #warning TODO:   dragging commits to their current location -> no repo changes
+//    #warning TODO:   same commit message -> no repo changes
+//    #warning TODO: no-op guarantee:
+//
+//    #warning TODO: make sure we can round-trip with the same date/time. especially test commits with negative UTC offsets!
+//    
+//    #warning TODO: bring back _CommitTime so we don't need to worry about the 'sign' field of git_time
+//
+//    #warning TODO: handle merge conflicts
+//
+//    #warning TODO: set_escdelay: not sure if we're going to encounter issues?
+//
+//    #warning TODO: don't allow combine when a merge commit is selected
+//
+//    #warning TODO: always show the same contextual menu, but gray-out the disabled options
+//    
+//    #warning TODO: make sure moving/deleting commits near the root commit still works
+//
+//    #warning TODO: properly handle moving/copying merge commits
+//
+//    #warning TODO: show some kind of indication that a commit is a merge commit
+//
+//    #warning TODO: handle window resizing
+//
+//    #warning TODO: re-evaluate size of drag-cancel affordance since it's not as reliable in iTerm
+//
+//    #warning TODO: don't allow double-click/return key on commits in read-only columns
+//
+//    #warning TODO: handle rewriting tags
+//    
+//    #warning TODO: create special color palette for apple terminal
+//
+//    #warning TODO: if can_change_color() returns false, use default color palette (COLOR_RED, etc)
+//
+//    #warning TODO: fix: colors aren't restored when exiting
+//
+//    #warning TODO: figure out whether/where/when to call git_libgit2_shutdown()
+//
+//    #warning TODO: implement error messages
+//
+//    #warning TODO: double-click broken: click commit, wait, then double-click
+//
+//    #warning TODO: implement double-click to edit
+//
+//    #warning TODO: implement key combos for combine/edit
+//
+//    #warning TODO: show indication in the UI that a column is immutable
+//
+//    #warning TODO: special-case opening `mate` when editing commit, to not call CursesDeinit/CursesInit
+//
+//    #warning TODO: implement EditCommit
+//
+//    #warning TODO: allow editing author/date of commit
+//    
+//    #warning TODO: configure tty before/after calling out to a new editor
+//    
+//    #warning TODO: fix commit message rendering when there are newlines
+//
+//    #warning TODO: improve panel dragging along edges. currently the shadow panels get a little messed up
+//
+//    #warning TODO: add affordance to cancel current drag by moving mouse to edge of terminal
+//
+//    #warning TODO: implement CombineCommits
+//
+//    #warning TODO: draw "Move/Copy" text immediately above the dragged commits, instead of at the insertion point
+//    
+//    #warning TODO: create concept of revs being mutable. if they're not mutable, don't allow moves from them (only copies), moves to them, deletes, etc
+//    
+//    #warning TODO: don't allow remote branches to be modified (eg 'origin/master')
+//    
+//    #warning TODO: show similar commits to the selected commit using a lighter color
+//    
+//    #warning       to do so, we'll need to implement operator< on Rev so we can put them in a set
+//    #warning TODO: we need to unique-ify the supplied revs, since we assume that each column has a unique rev
+//    
+//    #warning TODO: allow escape key to abort a drag
+//    
+//    #warning TODO: when copying commmits, don't hide the source commits
     
 //    {
 //        volatile bool a = false;
