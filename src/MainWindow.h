44b0f18caf2853eb93335dcaeb17638e611510ad
Dave <dave@heytoaster.com>
2022-04-02 14:01:23 -0700

    MainWindow: cleanup

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -296,7 +296,7 @@ public:
     void run() {
         namespace fs = std::filesystem;
         
-        _Theme = _ThemeRead();
+        _theme = _ThemeRead();
         _head = _repo.head();
         
         // Create _repoState
@@ -344,9 +344,10 @@ public:
         );
         
         try {
-            _CursesInit();
-            Defer(_CursesDeinit());
+            _cursesInit();
+            Defer(_cursesDeinit());
             
+            // Create our window now that ncurses is initialized
             Window::operator=(Window(::stdscr));
             
             for (;;) {
@@ -403,6 +404,123 @@ private:
         }
         // Programmer error if it doesn't exist
         abort();
+    }
+    
+    static UI::Color _ColorSet(const UI::Color& c) {
+        UI::Color prev(c.idx);
+        color_content(prev.idx, &prev.r, &prev.g, &prev.b);
+        ::init_color(c.idx, c.r, c.g, c.b);
+        return prev;
+    }
+    
+    static UI::ColorPalette _ColorsSet(const UI::ColorPalette& p) {
+        UI::ColorPalette pcopy = p;
+        
+        // Set the values for the custom colors, and remember the old values
+        for (UI::Color& c : pcopy.custom()) {
+            c = _ColorSet(c);
+        }
+        
+        for (const UI::Color& c : p.colors()) {
+            ::init_pair(c.idx, c.idx, -1);
+        }
+        
+        return pcopy;
+    }
+    
+    static UI::ColorPalette _ColorsCreate(State::Theme theme) {
+        const std::string termProg = getenv("TERM_PROGRAM");
+        const bool themeDark = (theme==State::Theme::None || theme == State::Theme::Dark);
+        
+        UI::ColorPalette colors;
+        UI::Color black;
+        UI::Color gray;
+        UI::Color purple;
+        UI::Color purpleLight;
+        UI::Color greenMint;
+        UI::Color red;
+        
+        if (termProg == "Apple_Terminal") {
+            // Colorspace: unknown
+            // There's no simple relation between these numbers and the resulting colors because Apple's
+            // Terminal.app applies some kind of filtering on top of these numbers. These values were
+            // manually chosen based on their appearance.
+            if (themeDark) {
+                colors.normal           = COLOR_BLACK;
+                colors.dimmed           = colors.add( 77,  77,  77);
+                colors.selection        = colors.add(  0,   2, 255);
+                colors.selectionSimilar = colors.add(140, 140, 255);
+                colors.selectionCopy    = colors.add(  0, 229, 130);
+                colors.menu             = colors.selectionCopy;
+                colors.error            = colors.add(194,   0,  71);
+            
+            } else {
+                colors.normal           = COLOR_BLACK;
+                colors.dimmed           = colors.add(128, 128, 128);
+                colors.selection        = colors.add(  0,   2, 255);
+                colors.selectionSimilar = colors.add(140, 140, 255);
+                colors.selectionCopy    = colors.add( 52, 167,   0);
+                colors.menu             = colors.add(194,   0,  71);
+                colors.error            = colors.menu;
+            }
+        
+        } else {
+            // Colorspace: sRGB
+            // These colors were derived by sampling the Apple_Terminal values when they're displayed on-screen
+            
+            if (themeDark) {
+                colors.normal           = COLOR_BLACK;
+                colors.dimmed           = colors.add(.486*255, .486*255, .486*255);
+                colors.selection        = colors.add(.463*255, .275*255, 1.00*255);
+                colors.selectionSimilar = colors.add(.663*255, .663*255, 1.00*255);
+                colors.selectionCopy    = colors.add(.204*255, .965*255, .569*255);
+                colors.menu             = colors.selectionCopy;
+                colors.error            = colors.add(.969*255, .298*255, .435*255);
+            
+            } else {
+                colors.normal           = COLOR_BLACK;
+                colors.dimmed           = colors.add(.592*255, .592*255, .592*255);
+                colors.selection        = colors.add(.369*255, .208*255, 1.00*255);
+                colors.selectionSimilar = colors.add(.627*255, .627*255, 1.00*255);
+                colors.selectionCopy    = colors.add(.306*255, .737*255, .153*255);
+                colors.menu             = colors.add(.969*255, .298*255, .435*255);
+                colors.error            = colors.menu;
+            }
+        }
+        
+        return colors;
+    }
+    
+    State::Theme _ThemeRead() {
+        State::State state(StateDir());
+        State::Theme theme = state.theme();
+        if (theme != State::Theme::None) return theme;
+        
+        bool write = false;
+        // If a theme isn't set, ask the terminal for its background color,
+        // and we'll choose the theme based on that
+        Terminal::Background bg = Terminal::Background::Dark;
+        try {
+            bg = Terminal::BackgroundGet();
+        } catch (...) {
+            // We failed to get the terminal background color, so write the theme
+            // for the default background color to disk, so we don't try to get
+            // the background color again in the future. (This avoids a timeout
+            // delay in Terminal::BackgroundGet() that occurs if the terminal
+            // doesn't support querying the background color.)
+            write = true;
+        }
+        
+        switch (bg) {
+        case Terminal::Background::Dark:    theme = State::Theme::Dark; break;
+        case Terminal::Background::Light:   theme = State::Theme::Light; break;
+        }
+        
+        if (write) {
+            state.theme(theme);
+            state.write();
+        }
+        return theme;
     }
     
     _SelectState _selectStateGet(RevColumnPtr col, CommitPanelPtr panel) {
@@ -1012,104 +1130,7 @@ private:
         _reload();
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    static UI::Color _ColorSet(const UI::Color& c) {
-        UI::Color prev(c.idx);
-        color_content(prev.idx, &prev.r, &prev.g, &prev.b);
-        ::init_color(c.idx, c.r, c.g, c.b);
-        return prev;
-    }
-
-    static UI::ColorPalette _ColorsSet(const UI::ColorPalette& p) {
-        UI::ColorPalette pcopy = p;
-        
-        // Set the values for the custom colors, and remember the old values
-        for (UI::Color& c : pcopy.custom()) {
-            c = _ColorSet(c);
-        }
-        
-        for (const UI::Color& c : p.colors()) {
-            ::init_pair(c.idx, c.idx, -1);
-        }
-        
-        return pcopy;
-    }
-
-    static UI::ColorPalette _ColorsCreate(State::Theme theme) {
-        const std::string termProg = getenv("TERM_PROGRAM");
-        const bool themeDark = (theme==State::Theme::None || theme == State::Theme::Dark);
-        
-        UI::ColorPalette colors;
-        UI::Color black;
-        UI::Color gray;
-        UI::Color purple;
-        UI::Color purpleLight;
-        UI::Color greenMint;
-        UI::Color red;
-        
-        if (termProg == "Apple_Terminal") {
-            // Colorspace: unknown
-            // There's no simple relation between these numbers and the resulting colors because Apple's
-            // Terminal.app applies some kind of filtering on top of these numbers. These values were
-            // manually chosen based on their appearance.
-            if (themeDark) {
-                colors.normal           = COLOR_BLACK;
-                colors.dimmed           = colors.add( 77,  77,  77);
-                colors.selection        = colors.add(  0,   2, 255);
-                colors.selectionSimilar = colors.add(140, 140, 255);
-                colors.selectionCopy    = colors.add(  0, 229, 130);
-                colors.menu             = colors.selectionCopy;
-                colors.error            = colors.add(194,   0,  71);
-            
-            } else {
-                colors.normal           = COLOR_BLACK;
-                colors.dimmed           = colors.add(128, 128, 128);
-                colors.selection        = colors.add(  0,   2, 255);
-                colors.selectionSimilar = colors.add(140, 140, 255);
-                colors.selectionCopy    = colors.add( 52, 167,   0);
-                colors.menu             = colors.add(194,   0,  71);
-                colors.error            = colors.menu;
-            }
-        
-        } else {
-            // Colorspace: sRGB
-            // These colors were derived by sampling the Apple_Terminal values when they're displayed on-screen
-            
-            if (themeDark) {
-                colors.normal           = COLOR_BLACK;
-                colors.dimmed           = colors.add(.486*255, .486*255, .486*255);
-                colors.selection        = colors.add(.463*255, .275*255, 1.00*255);
-                colors.selectionSimilar = colors.add(.663*255, .663*255, 1.00*255);
-                colors.selectionCopy    = colors.add(.204*255, .965*255, .569*255);
-                colors.menu             = colors.selectionCopy;
-                colors.error            = colors.add(.969*255, .298*255, .435*255);
-            
-            } else {
-                colors.normal           = COLOR_BLACK;
-                colors.dimmed           = colors.add(.592*255, .592*255, .592*255);
-                colors.selection        = colors.add(.369*255, .208*255, 1.00*255);
-                colors.selectionSimilar = colors.add(.627*255, .627*255, 1.00*255);
-                colors.selectionCopy    = colors.add(.306*255, .737*255, .153*255);
-                colors.menu             = colors.add(.969*255, .298*255, .435*255);
-                colors.error            = colors.menu;
-            }
-        }
-        
-        return colors;
-    }
-
-    void _CursesInit() noexcept {
+    void _cursesInit() noexcept {
         // Default Linux installs may not contain the /usr/share/terminfo database,
         // so provide a fallback terminfo that usually works.
         nc_set_default_terminfo(xterm_256color, sizeof(xterm_256color));
@@ -1130,12 +1151,12 @@ private:
         ::start_color();
         
         if (can_change_color()) {
-            _colors = _ColorsCreate(_Theme);
+            _colors = _ColorsCreate(_theme);
         }
         
-        _ColorsPrev = _ColorsSet(_colors);
+        _colorsPrev = _ColorsSet(_colors);
         
-        _CursorState = UI::CursorState(false, {});
+        _cursorState = UI::CursorState(false, {});
         
     //    // Hide cursor
     //    ::curs_set(0);
@@ -1146,12 +1167,12 @@ private:
         ::set_escdelay(0);
     }
 
-    void _CursesDeinit() noexcept {
+    void _cursesDeinit() noexcept {
     //    ::mousemask(0, NULL);
         
-        _CursorState.restore();
+        _cursorState.restore();
         
-        _ColorsSet(_ColorsPrev);
+        _ColorsSet(_colorsPrev);
         ::endwin();
         
     //    sleep(1);
@@ -1167,7 +1188,7 @@ private:
         };
         
         const bool preserveTerminal = preserveTerminalCmds.find(argv[0]) != preserveTerminalCmds.end();
-        if (!preserveTerminal) _CursesDeinit();
+        if (!preserveTerminal) _cursesDeinit();
         
         // Spawn the text editor and wait for it to exit
         {
@@ -1183,65 +1204,19 @@ private:
             if (ir != pid) throw Toastbox::RuntimeError("unknown waitpid result: %d", ir);
         }
         
-        if (!preserveTerminal) _CursesInit();
+        if (!preserveTerminal) _cursesInit();
     }
-
-    static State::Theme _ThemeRead() {
-        State::State state(StateDir());
-        State::Theme theme = state.theme();
-        if (theme != State::Theme::None) return theme;
-        
-        bool write = false;
-        // If a theme isn't set, ask the terminal for its background color,
-        // and we'll choose the theme based on that
-        Terminal::Background bg = Terminal::Background::Dark;
-        try {
-            bg = Terminal::BackgroundGet();
-        } catch (...) {
-            // We failed to get the terminal background color, so write the theme
-            // for the default background color to disk, so we don't try to get
-            // the background color again in the future. (This avoids a timeout
-            // delay in Terminal::BackgroundGet() that occurs if the terminal
-            // doesn't support querying the background color.)
-            write = true;
-        }
-        
-        switch (bg) {
-        case Terminal::Background::Dark:    theme = State::Theme::Dark; break;
-        case Terminal::Background::Light:   theme = State::Theme::Light; break;
-        }
-        
-        if (write) {
-            state.theme(theme);
-            state.write();
-        }
-        return theme;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
     Git::Repo _repo;
     std::vector<Git::Rev> _revs;
     
     ColorPalette _colors;
-    UI::ColorPalette _ColorsPrev;
+    UI::ColorPalette _colorsPrev;
     State::RepoState _repoState;
     Git::Rev _head;
     std::vector<RevColumnPtr> _columns;
-    UI::CursorState _CursorState;
-    State::Theme _Theme = State::Theme::None;
+    UI::CursorState _cursorState;
+    State::Theme _theme = State::Theme::None;
     
     struct {
         CommitPanelPtr titlePanel;
