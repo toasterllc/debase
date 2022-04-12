23771300054a476ecdfb039f8ee8249603ef3d9e
Dave <dave@heytoaster.com>
2022-04-12 02:18:14 -0700

    Cleanup

--- src/ui/SnapshotButton.h
+++ src/ui/SnapshotButton.h
@@ -51,60 +51,9 @@ public:
     }
     
     void draw() override {
-        const int width = size().x;
-        const Size offTextX = Size{_TextInsetX, 0};
-        const Size offTextY = Size{0, 0};
-        const Size offText = offTextX+offTextY;
-        
-//        // Draw time
-//        {
-//            Attr color = attr(Colors().dimmed);
-//            int offX = width - (int)UTF8::Len(_time);
-//            drawText(offTextY + Size{offX, 0}, _time.c_str());
-//        }
-        
-        const Color idColor = ((highlighted() || (_activeSnapshot && !mouseActive())) ? Colors().menu : Colors().normal);
+        const bool menuColor = (highlighted() || (_activeSnapshot && !mouseActive()));
+        const Color idColor = (menuColor ? Colors().menu : Colors().normal);
         _id->textAttr(idColor|A_BOLD);
-        
-//        // Draw commit id
-//        {
-//            Attr bold = attr(A_BOLD);
-//            Attr color;
-//            if (highlighted() || (activeSnapshot && !mouseActive())) {
-//                color = attr(Colors().menu);
-//            }
-//            drawText(offText, _commit.id.c_str());
-//        }
-        
-//        // Draw author name
-//        {
-//            Attr color = attr(Colors().dimmed);
-//            drawText(offText + Size{0, 1}, _commit.author.c_str());
-//        }
-        
-//        // Draw commit message
-//        {
-//            int i = 0;
-//            for (const std::string& line : _commit.message) {
-//                drawText(offText + Size{0, 2+i}, line.c_str());
-//                i++;
-//            }
-//        }
-        
-//        // Draw highlight
-//        if (highlighted()) {
-////            _highlightSymbol->textAttr(Colors().menu|A_BOLD);
-//            _highlightSymbol->text("A");
-//            os_log(OS_LOG_DEFAULT, "AAA");
-////            _highlightSymbol->drawNeeded(true);
-////            _highlightSymbol->eraseNeeded(false);
-//        } else {
-////            _highlightSymbol->textAttr(Colors().normal);
-//            _highlightSymbol->text("B");
-//            os_log(OS_LOG_DEFAULT, "BBB");
-////            _highlightSymbol->drawNeeded(true);
-////            _highlightSymbol->eraseNeeded(false);
-//        }
         
         // Draw highlight
         if (highlighted() || (_activeSnapshot && !mouseActive())) {
