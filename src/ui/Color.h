de5e902ebe75534c8a3723db7b920592c91b1d32
Dave <dave@heytoaster.com>
2022-04-13 23:44:16 -0700

    Cleanup

--- src/ui/Color.h
+++ src/ui/Color.h
@@ -5,110 +5,102 @@
 
 namespace UI {
 
-struct RGB {
-    int r = 0;
-    int g = 0;
-    int b = 0;
-    bool operator ==(const RGB& x) const { return r==x.r && g==x.g && b==x.b; }
-};
-
-struct Color {
-    int idx = COLOR_BLACK;
-    std::optional<RGB> rgb;
+struct ColorIdx {
+    NCURSES_COLOR_T idx = COLOR_BLACK;
+//    RGB rgb;
     
-    bool operator ==(const Color& x) const { return idx==x.idx && rgb==x.rgb; }
+    ColorIdx() {}
+    ColorIdx(NCURSES_COLOR_T idx) : idx(idx) {}
+    operator NCURSES_COLOR_T() const { return idx; }
+//    bool operator ==(const Color& x) const { return idx==x.idx && rgb==x.rgb; }
 };
 
 struct ColorPair {
-    int idx = WhiteOnBlackIdx;
-    std::optional<int> fg;
-    std::optional<int> bg;
+    NCURSES_PAIRS_T idx = WhiteOnBlackIdx;
+    
+    ColorPair() {}
+    ColorPair(NCURSES_PAIRS_T idx) : idx(idx) {}
     
     // WhiteOnBlackIdx: color-pair 0 is ncurses' immutable white-on-black color pair
-    static constexpr int WhiteOnBlackIdx = 0;
-    operator int() const { return COLOR_PAIR(idx); }
+    static constexpr NCURSES_PAIRS_T WhiteOnBlackIdx = 0;
+    operator NCURSES_PAIRS_T() const { return COLOR_PAIR(idx); }
 };
-
-
-
-
-
-
 
 class ColorSwapper {
 public:
     ColorSwapper() {}
-    ColorSwapper(const Color& c) {
-        assert(c.rgb);
-        
+    ColorSwapper(const ColorIdx& idx, NCURSES_COLOR_T r, NCURSES_COLOR_T g, NCURSES_COLOR_T b) : _s{.idx=idx} {
         // Remember the previous RGB values for color index `idx`
-        NCURSES_COLOR_T r=-1, g=-1, b=-1;
-        int ir = ::color_content(c.idx, &r, &g, &b);
+        int ir = ::color_content(*_s.idx, &_s.r, &_s.g, &_s.b);
         if (ir != OK) throw std::runtime_error("color_content() failed");
-        _prev = Color{.idx=c.idx, .rgb=RGB{r,g,b}};
         
         // Set the new RGB values for color index `idx`
-        ir = ::init_color(c.idx, c.rgb->r, c.rgb->g, c.rgb->b);
+        ir = ::init_color(*_s.idx, r, g, b);
         if (ir != OK) throw std::runtime_error("init_color() failed");
     }
     
     ColorSwapper(const ColorSwapper& x) = delete;
     ColorSwapper(ColorSwapper&& x) {
-        std::swap(_prev, x._prev);
+        std::swap(_s, x._s);
     }
     
     ColorSwapper& operator =(ColorSwapper&& x) {
-        std::swap(_prev, x._prev);
+        std::swap(_s, x._s);
         return *this;
     }
     
     ~ColorSwapper() {
-        if (_prev) {
-            ::init_color(_prev->idx, _prev->rgb->r, _prev->rgb->g, _prev->rgb->b);
+        if (_s.idx) {
+            ::init_color(*_s.idx, _s.r, _s.g, _s.b);
         }
     }
     
 private:
-    std::optional<Color> _prev;
+    struct {
+        std::optional<ColorIdx> idx;
+        NCURSES_COLOR_T r = -1;
+        NCURSES_COLOR_T g = -1;
+        NCURSES_COLOR_T b = -1;
+    } _s;
 };
 
 
 class ColorPairSwapper {
 public:
     ColorPairSwapper() {}
-    ColorPairSwapper(const ColorPair& p) {
-        assert(p.fg || p.bg);
-        
+    ColorPairSwapper(const ColorPair& pair, NCURSES_COLOR_T fg, NCURSES_COLOR_T bg) : _s{.pair=pair} {
         // Remember the previous fg/bg colors for the color pair c.idx
-        NCURSES_COLOR_T fg=-1, bg=-1;
-        int ir = ::pair_content(p.idx, &fg, &bg);
+        int ir = ::pair_content(*_s.pair, &_s.fg, &_s.bg);
         if (ir != OK) throw std::runtime_error("pair_content() failed");
-        _prev = {.idx=p.idx, .fg=fg, .bg=bg};
         
         // Set the new RGB values for the color `c.idx`
-        ir = ::init_pair(p.idx, (p.fg ? *p.fg : -1), (p.bg ? *p.bg : -1));
+        ir = ::init_pair(*_s.pair, fg, bg);
         if (ir != OK) throw std::runtime_error("init_pair() failed");
     }
     
     ColorPairSwapper(const ColorPairSwapper& x) = delete;
     
     ColorPairSwapper(ColorPairSwapper&& x) {
-        std::swap(_prev, x._prev);
+        std::swap(_s, x._s);
     }
     
     ColorPairSwapper& operator =(ColorPairSwapper&& x) {
-        std::swap(_prev, x._prev);
+        std::swap(_s, x._s);
         return *this;
     }
     
     ~ColorPairSwapper() {
-        if (_prev) {
-            ::init_pair(_prev->idx, *_prev->fg, *_prev->bg);
+        if (_s.pair) {
+            ::init_pair(*_s.pair, _s.fg, _s.bg);
         }
     }
     
 private:
-    std::optional<ColorPair> _prev;
+    struct {
+        std::optional<ColorPair> pair;
+        NCURSES_COLOR_T fg = -1;
+        NCURSES_COLOR_T bg = -1;
+    } _s;
 };
 
 class ColorPalette {
@@ -122,19 +114,19 @@ public:
     ColorPair error;
     
     // add(): create a new color pair with a given color index as the foreground color
-    ColorPair add(int idx) {
-        const ColorPair colorPair = {.idx=_colorPairIdx, .fg=idx};
-        _colorPairSwappers.emplace_back(colorPair);
-        _colorPairIdx++;
-        return colorPair;
+    ColorPair add(NCURSES_COLOR_T colorIdx) {
+        _colorPairSwappers.emplace_back(_colorPairIdx, colorIdx, -1);
+        return _colorPairIdx++;
     }
     
     // add(): create a new color with the given RGB components, and assign it to a new color pair as the foreground color
     ColorPair add(uint8_t r, uint8_t g, uint8_t b) {
-        const Color color = {.idx=_colorIdx, .rgb=RGB{((int)r*1000)/255, ((int)g*1000)/255, ((int)b*1000)/255}};
-        _colorSwappers.emplace_back(color);
-        _colorIdx++;
-        return add(color.idx);
+        _colorSwappers.emplace_back(_colorIdx,
+            ((NCURSES_COLOR_T)r*1000)/255,
+            ((NCURSES_COLOR_T)g*1000)/255,
+            ((NCURSES_COLOR_T)b*1000)/255
+        );
+        return add(_colorIdx++);
     }
     
 private:
@@ -144,10 +136,10 @@ private:
     // restore colors on exit, we won't necessarily be restoring the original
     // color. So if we're going to clobber colors, clobber the colors that are less
     // likely to be used.
-    static constexpr int _ColorIdxInit = 16;
+    static constexpr NCURSES_COLOR_T _ColorIdxInit = 16;
     
-    int _colorIdx = _ColorIdxInit;
-    int _colorPairIdx = 1;
+    NCURSES_COLOR_T _colorIdx = _ColorIdxInit;
+    NCURSES_PAIRS_T _colorPairIdx = 1;
     
     std::vector<ColorSwapper> _colorSwappers;
     std::vector<ColorPairSwapper> _colorPairSwappers;
