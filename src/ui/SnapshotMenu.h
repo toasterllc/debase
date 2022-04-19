7553019f28e94ae25c8e2a1eb74781ec088273e7
Dave <dave@heytoaster.com>
2022-04-19 04:04:06 -0700

    Fix cchar_t creation

--- src/ui/SnapshotMenu.h
+++ src/ui/SnapshotMenu.h
@@ -21,8 +21,16 @@ public:
                 Attr color = attr(colors().menu);
                 Point p = {1, button0->frame().b()};
                 int len = width-2;
-                cchar_t c = { .attr=0, { .chars = L"╍" } };
-                mvwhline_set(*this, p.y, p.x, &c, len);
+                
+                constexpr cchar_t Chars = { 0, L"╍" };
+//                cchar_t c = {};
+//                const wchar_t chars[] = L"╍";
+//                static_assert(sizeof(chars) <= sizeof(c.chars));
+//                // GCC has a weird bug where we can't initialize `c` with designated-initializer
+//                // syntax, without using extra braces that Clang can't compile. So just memcpy it.
+//                memcpy(c.chars, chars, sizeof(chars));
+                
+                mvwhline_set(*this, p.y, p.x, &Chars, len);
             }
         }
     }
