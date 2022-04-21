2e6674899b146ab1854322c170f11d53ccead4ea
Dave <dave@heytoaster.com>
2022-04-21 02:10:12 -0700

    Remove src/Obfuscation.h since stripping the binary does the job

--- src/Obfuscation.h
+++
@@ -1,8 +0,0 @@
-#define Machine             Git
-#define MachineId           GitHash
-#define MachineIdCalc       GitHashCalc
-#define MachineIdBasicCalc  GitHashBasicCalc
-#define MachineInfo         GitInfo
-#define MachineInfoCalc     GitInfoCalc
-#define RAMCapacity         GitCapacity
-#define _SerialGet          _HashGet
